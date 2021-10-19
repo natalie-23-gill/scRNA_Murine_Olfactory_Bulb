4c9f875ed1bb685bdae438f02abc477fd9239167
Natalie Elphick <nelphick@uoregon.edu>
2021-10-18 17:46:28 -0700

    Updated script to rename outputs correctly and zip them automatically

--- Preprocessing/combine_files/combine_files.sh
+++ Preprocessing/combine_files/combine_files.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+
+
 #Created: 10/08/2021
 #Author: Natalie Elphick
 #Script to combine files across lanes and flow-cells so all Read 1s and Read 2s
 #from Library LXXXXX are within a single FASTQ file.
 
 
-#/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/flowcell_1
-#/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/flowcell_2
 
-dir="/projects/bgmp/nelphick/bioinfo/Yu_project/Yu_Project_2021/Preprocessing/combine_files/test_files/"
+# Test files
+# /projects/bgmp/nelphick/bioinfo/Yu_project/Yu_Project_2021/Preprocessing/combine_files/test_files/
+
+
+dir="/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/"
 f1="flowcell_1"
 f2="flowcell_2"
 samples="$(ls -1 $dir$f1 | grep -v '.txt$')"
@@ -22,8 +31,10 @@ for flowcell in $f1 $f2
     do
     for sample in $samples
         do
-        zcat "${flowcell}/${sample}/"*R1* >> "${out_dir}/${sample}_R1.fastq"
-        zcat "${flowcell}/${sample}/"*R2* >> "${out_dir}/${sample}_R2.fastq"
+        zcat "${flowcell}/${sample}/"*R1* >> "${out_dir}/${sample}_S1_L001_R1_001.fastq"
+        zcat "${flowcell}/${sample}/"*R2* >> "${out_dir}/${sample}_S1_L001_R2_001.fastq"
     done
 done
 
+cd $out_dir
+pigz -p 16 *

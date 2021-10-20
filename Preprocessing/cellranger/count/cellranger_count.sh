9fda4a25be647b1860922a23eae04a373cd09825
Natalie Elphick <nelphick@uoregon.edu>
2021-10-20 09:29:37 -0700

    modifiy the cell ranger count script to run all 10 samples

--- Preprocessing/cellranger/count/cellranger_count.sh
+++ Preprocessing/cellranger/count/cellranger_count.sh
@@ -15,15 +15,19 @@
 dir="/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/"
 out_dir="cr_count_output"
 
+samples = "L35463 L35464 L35465 L35466 L35467 L35468 L35469 L35470 L35472 L35471"
+
 cd $dir
 mkdir $out_dir
 cd $out_dir
 
+for sample in $samples
+    do
+        /usr/bin/time -v cellranger count --id=sample_$sample \
+        --transcriptome=/projects/bgmp/shared/2021_projects/Yu/cellranger_build/Mus_musculus.GRCm39.dna.ens104 \
+        --fastqs=/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/combined_files_output \
+        --sample=$sample \
+        --localcores=16
+done
 
-/usr/bin/time -v cellranger count --id=sample_L35463 \
---transcriptome=/projects/bgmp/shared/2021_projects/Yu/cellranger_build/Mus_musculus.GRCm39.dna.ens104 \
---fastqs=/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/combined_files_output \
---sample=L35463 \
---localcores=16
-
-pigz -p 16 -r sample_L35463
+pigz -p 16 -r sample_*

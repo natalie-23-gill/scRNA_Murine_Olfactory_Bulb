c60d6a64e58a9d2a0d6c2b3fc70488a9385e1810
Christina Zakarian <czakarian@github.com>
2021-10-21 13:15:12 -0700

    updated cellranger_count.sh and added out/err files for the full run

--- Preprocessing/cellranger/count/cellranger_count.sh
+++ Preprocessing/cellranger/count/cellranger_count.sh
@@ -15,7 +15,7 @@
 dir="/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/"
 out_dir="cr_count_output"
 
-samples = "L35463 L35464 L35465 L35466 L35467 L35468 L35469 L35470 L35472 L35471"
+samples="L35463 L35464 L35465 L35466 L35467 L35468 L35469 L35470 L35472 L35471"
 
 cd $dir
 mkdir $out_dir
@@ -23,11 +23,12 @@ cd $out_dir
 
 for sample in $samples
     do
-        /usr/bin/time -v cellranger count --id=sample_$sample \
-        --transcriptome=/projects/bgmp/shared/2021_projects/Yu/cellranger_build/Mus_musculus.GRCm39.dna.ens104 \
-        --fastqs=/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/combined_files_output \
-        --sample=$sample \
-        --localcores=16
+    /usr/bin/time -v cellranger count \
+    --id=sample_$sample \
+    --transcriptome=/projects/bgmp/shared/2021_projects/Yu/cellranger_build/Mus_musculus.GRCm39.dna.ens104 \
+    --fastqs=/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/combined_files_output \
+    --sample=$sample \
+    --localcores=16
 done
 
 pigz -p 16 -r sample_*

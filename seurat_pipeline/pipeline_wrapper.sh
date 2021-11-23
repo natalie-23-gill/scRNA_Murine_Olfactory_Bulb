24368465c969884c690729ea333f5a8a96877edd
Natalie Elphick <nelphick@uoregon.edu>
2021-11-23 14:07:43 -0800

    update progress

--- seurat_pipeline/pipeline_wrapper.sh
+++ seurat_pipeline/pipeline_wrapper.sh
@@ -6,11 +6,15 @@
 
 #SBATCH --account=bgmp
 #SBATCH --partition=bgmp
-#SBATCH --output=kb_ref_%j.out
-#SBATCH --error=kb_ref_%j.err
-#SBATCH --cpus-per-task=8
+#SBATCH --output=seurat_pipeline_%j.out
+#SBATCH --error=seurat_pipeline_%j.err
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16G
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00:00
 
 conda activate scRNA_R
+
+/usr/bin/time -v Rscript QC_subset.R \
+-data_dir /home/natgill/BGMP/bioinformatics/Yu_Project_2021
 

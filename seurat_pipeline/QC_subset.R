bddb1b03bf8f9a75adefe7884ff266a466992be1
Natalie Elphick <nelphick@uoregon.edu>
2021-11-26 10:42:45 -0800

    Finished QC_subset.R, added run outputs and fixed mislabeled folder

--- seurat_pipeline/QC_subset.R
+++ seurat_pipeline/QC_subset.R
@@ -1,22 +1,2 @@
 #!/usr/bin/env Rscript
 library(Seurat)
-library(argparser)
-library(tidyverse)
-
-# Parse command line arguments 
-p = arg_parser("Filter a list of RDS seurat objects using the same cutoffs for each object")
-p = add_argument(p, "--data_dir", help="Directory containing the RDS objects to filter")
-#p = add_argument(p, "--samples", help="Samples csv with 2 columns |Timepoint|Sample|")
-#p = add_argument(p, "--max_count", help="Maximum cutoff for nCount_RNA")
-#p = add_argument(p, "--min_count", help="Minimum cutoff for nCount_RNA")
-#p = add_argument(p, "--min_features", help="Minimum cutoff for nFeature_RNA")
-#p = add_argument(p, "--max_features", help="Maximum cutoff for nFeature_RNA")
-#p = add_argument(p, "--max_mito", help="Maximum cutoff for percent mitochondrial genes")
-args = parse_args(p)
-
-#setwd(args$data_dir)
-
-#samples = read_csv(args$samples)
-
-print(args$data_dir)
-
--- seurat_pipeline/pipeline_wrapper.sh
+++ seurat_pipeline/pipeline_wrapper.sh
@@ -6,15 +6,11 @@
 
 #SBATCH --account=bgmp
 #SBATCH --partition=bgmp
-#SBATCH --output=seurat_pipeline_%j.out
-#SBATCH --error=seurat_pipeline_%j.err
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=16G
+#SBATCH --output=kb_ref_%j.out
+#SBATCH --error=kb_ref_%j.err
+#SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00:00
 
 conda activate scRNA_R
-
-/usr/bin/time -v Rscript QC_subset.R \
--data_dir /home/natgill/BGMP/bioinformatics/Yu_Project_2021
 

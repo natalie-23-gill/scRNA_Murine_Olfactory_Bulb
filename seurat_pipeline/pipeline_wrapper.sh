585c1db74a3b203d11afc963df9f4ca64f5ef449
Natalie Elphick <nelphick@uoregon.edu>
2021-11-26 12:15:45 -0800

    Update main seurat pipeline and add current run info

--- seurat_pipeline/pipeline_wrapper.sh
+++ seurat_pipeline/pipeline_wrapper.sh
@@ -2,15 +2,26 @@
 
 # Created: 10/23/2021
 # Author: Natalie Elphick
-# wrapper script for seurat integrated analysis
+# wrapper script for seurat intergrated analysis pipeline
 
 #SBATCH --account=bgmp
 #SBATCH --partition=bgmp
-#SBATCH --output=kb_ref_%j.out
-#SBATCH --error=kb_ref_%j.err
-#SBATCH --cpus-per-task=8
+#SBATCH --output=seurat_pipeline_%j.out
+#SBATCH --error=seurat_pipeline_%j.err
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16G
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00:00
 
 conda activate scRNA_R
 
+/usr/bin/time -v Rscript QC_subset.R \
+/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/seurat_pipeline/kb_rds \
+/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/seurat_pipeline/kb_rds_filtered \
+/projects/bgmp/nelphick/bioinfo/Yu_project/Yu_Project_2021/data/timepoints.csv \
+10000 200 7
+
+/usr/bin/time -v Rscript SCT_integrate.R \
+/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/seurat_pipeline/kb_rds_filtered \
+/projects/bgmp/nelphick/bioinfo/Yu_project/Yu_Project_2021/data/timepoints.txt \
+/projects/bgmp/shared/2021_projects/Yu/BGMP_2021/seurat_pipeline/combined_kb_filtered.rds

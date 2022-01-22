fd1416c06533195d50172bea55a5aba138696643
Natalie Elphick <nelphick@uoregon.edu>
2022-01-22 14:44:01 -0800

    Revert to original filtering scheme

--- Clustering/seurat_pipeline/QC_subset.R
+++ Clustering/seurat_pipeline/QC_subset.R
@@ -42,51 +42,11 @@ for (i in 1:length(seurat_obj_kb)){
   
 }
 
-# Sample Specific Filtering
-# E14
-seurat_obj_kb[[1]]<- subset(seurat_obj_kb[[1]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 12000 & percent.mt < 6)
-
-# E18
-seurat_obj_kb[[2]] <- subset(seurat_obj_kb[[2]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 6)
-
-# P0
-seurat_obj_kb[[3]] <- subset(seurat_obj_kb[[3]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 15000 & percent.mt < 9)
-
-# P3
-seurat_obj_kb[[4]] <- subset(seurat_obj_kb[[4]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 12000 & percent.mt < 7.5)
-
-# P5
-seurat_obj_kb[[5]] <- subset(seurat_obj_kb[[5]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 8000 & percent.mt < 7)
-
-# P7
-seurat_obj_kb[[6]] <- subset(seurat_obj_kb[[6]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 8)
-
-
-# P10
-seurat_obj_kb[[7]] <- subset(seurat_obj_kb[[7]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 9)
-
-# P14
-seurat_obj_kb[[8]] <- subset(seurat_obj_kb[[8]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 13)
-
-# P21
-seurat_obj_kb[[9]] <- subset(seurat_obj_kb[[9]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 10)
-
-# Adult
-seurat_obj_kb[[10]] <- subset(seurat_obj_kb[[10]],
-                             subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 12)
-
-
+print("Post filtering Cell Counts:")
 for (i in 1:length(seurat_obj_kb)){
-
+  
+  seurat_obj_kb[[i]] <- subset(seurat_obj_kb[[i]],
+                              subset = nFeature_RNA > 200 & nCount_RNA < 10000 & percent.mt < 7)
 
   print(paste(timepoints[i],dim(seurat_obj_kb[[i]])[2]))
 
